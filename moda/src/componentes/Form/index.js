998dd80353930a36317e87193a8ca2193589b531
JM-arlopes <jmiguelarlopes@gmail.com>
2023-08-16 23:54:44 -0300

    inserindo funções nos props

--- moda/src/componentes/Form/index.js
+++ moda/src/componentes/Form/index.js
@@ -2,25 +2,53 @@ import TextArea from "../TextArea/TextArea"
 import ListaSuspensa from '../ListaSuspensa'
 import Btn from '../Btn'
 import './form.css'
-
-
-const kiplin = event => {
-    event.preventDefault()
-    console.log(event.target)
-}
-
-
+import { useState } from "react"
 
 const Form = () => {
     const roupas = ['vestidos', 'camisas', 'shorts', 'calças'];
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+
+    const kiplin = event => {
+        event.preventDefault()
+        console.log(event.target)
+    }
+
+    function alteracao(valor) {
+        setNome(valor)
+    }
 
     return (
         <section className="formulario">
             <form onSubmit={kiplin}>
                 <h2>Preencha todos os dados no Card abaixo para o submit</h2>
-                <TextArea obrigatorio={true} label="Nome:" placeholder="Informe seu nome?" />
-                <TextArea obrigatorio={true} label="Cargo:" placeholder="Informe seu cargo?" />
-                <TextArea label="Imagem:" placeholder="Informe o endereço de imagem" />
+                <TextArea
+                    obrigatorio={true}
+                    label="Nome:"
+                    placeholder="Informe seu nome?"
+                    valor={nome}
+                    aoAlterado={alteracao}
+                />
+
+                <TextArea
+                    obrigatorio={true}
+                    label="Cargo:"
+                    placeholder="Informe seu cargo?"
+                    // aqui fiz do jeito mais complexo porém mais sintatico e facil
+                    aoAlterado={valor => setCargo(valor)}
+                    valor={cargo}
+                />
+
+                <TextArea
+                    label="Imagem:"
+                    placeholder="Informe o endereço de imagem"
+                    // mesmo caso do comentario anterior
+                    aoAlterado={valor => setImagem(valor)}
+                    valor={imagem}
+                />
+
                 <ListaSuspensa obrigatorio={true} label="Veja nossos Profissionais" itens={roupas} />
                 <Btn>
                     Kiplin
--- moda/src/componentes/TextArea/TextArea.js
+++ moda/src/componentes/TextArea/TextArea.js
@@ -1,22 +1,18 @@
 import './TextArea.css'
 
 
-
-
-
 const TextArea = (props) => {
 
-    let valor = 'kkj'
-
-    const aoDigitado = (event) => {
-        valor = event.target.value
-    }
-
     const text = `${props.placeholder}...`
+
+    // caso tenha dúvidas, é como se jogasse a função "aoAlterado" pra ca, assim como 
+    // da pra jogar variáveis e strings por meio do props 
+    const aoDigitado = event => props.aoAlterado(event.target.value)
+
     return (
         <div className='text-area'>
             <label>{props.label}</label>
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={text}></input>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={text}></input>
         </div>
     )
 }
